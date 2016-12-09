40d7cb6d083038a34489449b2b80f6d8b35bad93
Sam <samowen62@yahoo.com>
2016-12-08 20:13:50 -0600

    Added working start menu

---
+++ Scripts/MenuSelection/MainMenu.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    private Button startGameButton;
+    private Button exitGameButton;
+
+    private const string START_GAME_NAME = "StartGame";
+    private const string END_GAME_BUTTON = "EndGame";
+
+    private bool startBlocked = false;
+    private AsyncOperation async = null;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("Death Race Game Start!!");
+        Button [] buttons = FindObjectsOfType<Button>();
+
+        foreach(var button in buttons)
+        {
+            switch (button.name)
+            {
+                case START_GAME_NAME:
+                    startGameButton = button;
+                    startGameButton.onClick.AddListener(delegate () { this.startButtonClicked(); });
+                    break;
+                case END_GAME_BUTTON:
+                    exitGameButton = button;
+                    exitGameButton.onClick.AddListener(delegate () { this.endGameButtonClicked(); });
+                    break;
+                default:
+                    Debug.LogWarning("unused button component: " + button.name);
+                    break;
+            }
+        }
+        
+        
+    }
+ 
+    private void startButtonClicked()
+    {
+        if (!startBlocked)
+        {
+            startBlocked = true;
+            Debug.Log("Started Test Track sequence");
+
+            SyncLoadLevel("paperEngine");
+        }
+    }
+
+    public void endGameButtonClicked()
+    {
+        Debug.Log("Exiting Game");
+        Application.Quit();
+    }
+
+    private void SyncLoadLevel(string levelName)
+    {
+        async = SceneManager.LoadSceneAsync(levelName);
+        Load();
+    }
+
+    IEnumerator Load()
+    {
+        Debug.Log("progress: " + async.progress);
+        yield return async;
+    }
+}
---
+++ Scripts/MenuSelection/OptionHover.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class OptionHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    private Text thisText;
+    private Color initialColor;
+    private int initialSize;
+    private int hoverSize;
+
+    public Color hoverColor;
+
+    void Start()
+    {
+        thisText = GetComponent<Text>();
+        initialColor = thisText.color;
+        initialSize = thisText.fontSize;
+        hoverSize = initialSize + 2;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        thisText.color = hoverColor;
+        thisText.fontSize = hoverSize;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        thisText.color = initialColor;
+        thisText.fontSize = initialSize;
+    }
+}
