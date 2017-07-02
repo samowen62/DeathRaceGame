cc96146a8073186bbc01cf4469dea9df85365d8d
Sam <samowen62@yahoo.com>
2017-07-02 15:26:25 -0500

    added controls for bezier curve creation. Will be used for camera path creation

---
+++ Scripts/Bezier/Editor/BeizerCurveInspector.cs
@@ -0,0 +1,172 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(BezierSpline))]
+public class BezierCurveInspector : Editor
+{
+
+    private BezierSpline spline;
+    private Transform handleTransform;
+    private Quaternion handleRotation;
+
+    private const float directionScale = 0.5f;
+    private const float handleSize = 0.04f;
+    private const float pickSize = 0.06f;
+
+    private static Color[] modeColors = {
+        Color.white,
+        Color.yellow,
+        Color.cyan
+    };
+
+    private int selectedIndex = -1;
+
+    private void OnSceneGUI()
+    {
+        spline = target as BezierSpline;
+        handleTransform = spline.transform;
+        handleRotation = Tools.pivotRotation == PivotRotation.Local ?
+            handleTransform.rotation : Quaternion.identity;
+
+        Vector3 p0 = ShowPoint(0);
+        for (int i = 1; i < spline.ControlPointCount; i += 3)
+        {
+            Vector3 p1 = ShowPoint(i);
+            Vector3 p2 = ShowPoint(i + 1);
+            Vector3 p3 = ShowPoint(i + 2);
+
+            Handles.color = Color.gray;
+            Handles.DrawLine(p0, p1);
+            Handles.DrawLine(p2, p3);
+
+            Handles.DrawBezier(p0, p3, p1, p2, Color.white, null, 2f);
+            p0 = p3;
+        }
+
+        ShowDirections();
+        ShowNormals();
+    }
+
+    public override void OnInspectorGUI()
+    {
+        //DrawDefaultInspector();
+        if (selectedIndex >= 0 && selectedIndex < spline.ControlPointCount)
+        {
+            DrawSelectedPointInspector();
+        }
+
+        if(spline == null)
+        {
+            //not sure why happening yet
+            Debug.Log("ERROR: spline is null");
+            return;
+        }
+
+        EditorGUI.BeginChangeCheck();
+        bool loop = EditorGUILayout.Toggle("Loop", spline.Loop);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(spline, "Toggle Loop");
+            EditorUtility.SetDirty(spline);
+            spline.Loop = loop;
+        }
+
+        spline = target as BezierSpline;
+        if (GUILayout.Button("Add Curve"))
+        {
+            Undo.RecordObject(spline, "Add Curve");
+            spline.AddCurve();
+            EditorUtility.SetDirty(spline);
+        }
+    }
+
+    private void ShowDirections()
+    {
+        Handles.color = Color.green;
+        Vector3 point = spline.GetPoint(0f);
+        Handles.DrawLine(point, point + spline.GetDirection(0f) * directionScale);
+        int lineSteps = spline.lineSteps;
+
+        for (int i = 1; i <= lineSteps; i++)
+        {
+            point = spline.GetPoint(i / (float)lineSteps);
+            Handles.DrawLine(point, point + spline.GetDirection(i / (float)lineSteps) * directionScale);
+        }
+    }
+
+    private void ShowNormals()
+    {
+        Handles.color = Color.red;
+        Vector3 point = spline.GetPoint(0f);
+        Handles.DrawLine(point, point + spline.GetUnitNormal(0f) * directionScale);
+        int lineSteps = 100;//set to 100 for debugging
+
+        for (int i = 1; i <= lineSteps; i++)
+        {
+            point = spline.GetPoint(i / (float)lineSteps);
+            Handles.DrawLine(point, point + spline.GetUnitNormal(i / (float)lineSteps) * directionScale);
+        }
+    }
+
+    private Vector3 ShowPoint(int index)
+    {
+        Vector3 point = handleTransform.TransformPoint(spline.GetControlPoint(index));
+        Handles.color = modeColors[(int)spline.GetControlPointMode(index)];
+
+        float size = HandleUtility.GetHandleSize(point);
+        if (index == 0)
+        {
+            size *= 2f;
+        }
+
+        /*
+         * The Button method both shows a button and returns whether it was clicked. So you typically 
+         * call it inside an if statement and perform the necessary work in the corresponding code block.
+         */
+        if (Handles.Button(point, handleRotation, size * handleSize, size * pickSize, Handles.DotCap))
+        {
+            selectedIndex = index;
+            Repaint();
+        }
+        if (selectedIndex == index)
+        {
+            EditorGUI.BeginChangeCheck();
+            point = Handles.DoPositionHandle(point, handleRotation);
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(spline, "Move Point");
+                EditorUtility.SetDirty(spline);
+                spline.SetControlPoint(index, handleTransform.InverseTransformPoint(point));
+            }
+        }
+        return point;
+    }
+
+    /*
+     * This method handles what should change when something is changed on the UI of the editor
+     */
+    private void DrawSelectedPointInspector()
+    {
+        GUILayout.Label("Selected Point");
+        EditorGUI.BeginChangeCheck();
+        Vector3 point = EditorGUILayout.Vector3Field("Position", spline.GetControlPoint(selectedIndex));
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(spline, "Move Point");
+            EditorUtility.SetDirty(spline);
+            spline.SetControlPoint(selectedIndex, point);
+        }
+
+        //for mode changing
+        EditorGUI.BeginChangeCheck();
+        BezierControlPointMode mode = (BezierControlPointMode)
+            EditorGUILayout.EnumPopup("Mode", spline.GetControlPointMode(selectedIndex));
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(spline, "Change Point Mode");
+            spline.SetControlPointMode(selectedIndex, mode);
+            EditorUtility.SetDirty(spline);
+        }
+    }
+}
