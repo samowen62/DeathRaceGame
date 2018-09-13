774699c8944a1a0d1e5db679ce6e7c65c21e3c3b
Sam <samowen62@yahoo.com>
2018-09-12 20:37:05 -0500

    Added spin script and nailed lighting on tube track

---
+++ Scripts/SpinScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class SpinScript : PausableBehaviour {
+
+    [SerializeField]
+    public float spinSpeed = 1f;
+
+    [SerializeField]
+    private int _choiceIndex = 0;
+    [HideInInspector]
+    public int ChoiceIndex
+    {
+        get
+        {
+            return _choiceIndex;
+        }
+        set
+        {
+            if (_choiceIndex == value) return;
+            _choiceIndex = value;
+        }
+    }
+
+    private Vector3 momentAxis { get; set; }
+
+    protected override void _update()
+    {
+        transform.RotateAround(transform.position, momentAxis, 6 * spinSpeed * Time.deltaTime);
+    }
+
+    protected override void _awake()
+    {
+        var choices = new[] { transform.up, transform.right, transform.forward };
+        momentAxis = choices[_choiceIndex];
+    }
+}
+
---
+++ Scripts/SpinScriptInspector.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(SpinScript))]
+public class SpinScriptInspector : Editor
+{
+    private string[] choiceLabels = new string[] { "x-axis", "y-axis", "z-axis" };
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        var spinner = target as SpinScript;
+        // Choose an option from the list
+        spinner.ChoiceIndex = EditorGUILayout.Popup(spinner.ChoiceIndex, choiceLabels);
+
+        EditorUtility.SetDirty(spinner);
+    }
+}
