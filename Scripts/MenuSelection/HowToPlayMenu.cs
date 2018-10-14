23f5a41232fca3b11f5c406bb4167899073fa4a5
Sam <samowen62@yahoo.com>
2018-10-14 11:31:39 -0500

    Created how to play menu

---
+++ Scripts/MenuSelection/HowToPlayMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HowToPlayMenu : MonoBehaviour
+{
+    private Button _backButton;
+    private AsyncOperation async = null;
+    
+    void Awake()
+    {
+        _backButton = GameObject.Find("BackButton").GetComponent<Button>();
+        _backButton.onClick.AddListener(delegate () { backButtonClicked(); });
+    }
+
+    private void backButtonClicked()
+    {
+        Debug.Log("Back to main");
+        LoadLevel(AppConfig.MENU_MAIN);
+    }
+
+    private void LoadLevel(string levelName)
+    {
+        this.SyncLoadLevel(levelName);
+    }
+}
